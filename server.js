bdeb06173905cdf9535518d6f7db8d6baa0166d4
Nikhila <131186535+nikiGN-07@users.noreply.github.com>
2024-10-06 18:53:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ app.get('/', (req, res) => {
   res.send('Dice Roller API is running.');
 });
 
-// REST API to roll dice
+// REST API to roll dice 5 times
 app.get('/roll-dice', (req, res) => {
-  const randomNumber = Math.floor(Math.random() * 6) + 1;
-  res.json({ result: randomNumber });
+  const rolls = [];
+  for (let i = 0; i < 5; i++) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1; // Roll the dice
+    rolls.push(randomNumber); // Store the result in the rolls array
+  }
+  res.json({ results: rolls }); // Return the results as a JSON response
 });
 
 // Start server
